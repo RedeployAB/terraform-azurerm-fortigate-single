7a117b1c2c73c85796c0c480fa92de666ce1b418
Karl Wallenius <karl.wallenius@redeploy.com>
2021-06-09 13:36:07 +0200

    Further clarification on the storage account blob uri

--- variables.tf
+++ variables.tf
@@ -233,6 +233,6 @@ variable "user_assigned_identity_id" {
 
 variable "boot_diagnostics_storage_account_uri" {
   type        = string
-  description = "URI to storage account blob endpoint for boot diagnostic files. Optional."
+  description = "URI to storage account primary blob endpoint for boot diagnostic files. Optional."
   default     = null
 }
